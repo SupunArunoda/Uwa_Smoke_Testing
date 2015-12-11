97e0489aabc1a421c6c9eb48052a8f9b298571c4
SupunArunoda <supunarunoda@gmail.com>
2015-12-11 18:25:38 +0530

    Smoking Test Initial

---
+++ index.php
@@ -0,0 +1,98 @@
+
+
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="en">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <link type="text/css" rel="stylesheet" href="bootstrap-3.3.6-dist/css/bootstrap.css">
+        <title>Attorney General</title>
+    </head>
+    <body>
+        <p><img src="http://www.brandsoftheworld.com/sites/default/files/styles/logo-thumbnail/public/042011/government_logo_of_sri_lanka-converted.png?itok=er9QPW5U" alt="headImg" width="152" height="129" /><a href="https://www.google.lk/url?sa=t&amp;rct=j&amp;q=&amp;esrc=s&amp;source=web&amp;cd=1&amp;sqi=2&amp;ved=0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA&amp;url=http%3A%2F%2Fwww.attorneygeneral.gov.lk%2F&amp;usg=AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA&amp;bvm=bv.109332125,d.d2s&amp;cad=rja" onmousedown="return rwt(this, '', '', '', '1', 'AFQjCNGi6TFu7bmnNUcgrj8PAhr2JHl6RA', '', '0ahUKEwiQ8Kv79s3JAhVBthoKHX6lDq4QFggbMAA', '', '', event)">Attorney General's Department - Sri Lanka</a>
+        </p>
+        <p>&nbsp;</p>
+        <div class="container">
+    <div class="row">
+        <form role="form" action="process.php" method="post">
+            <div class="col-lg-6">
+               <div class="well well-sm"><strong><span class="glyphicon glyphicon-asterisk"></span>Vehicle Details</strong></div>
+               <div class="form-group">
+                    
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="InputCustNameFirst" name="cust_name" placeholder="Customer Name" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    
+                    <div class="input-group">
+                        <input type="text" class="form-control" name="reg_no" id="InputName" placeholder="Registered Number" required>
+                    </div>
+                </div>
+               
+                <div class="form-group">
+                    
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="InputEmailFirst" name="eng_no" placeholder="Engine Number" required>
+                        
+                    </div>
+                </div>
+               
+                <div class="form-group">
+                    
+                    <div class="input-group">
+                        <input type="text" class="form-control" id="InputEmailSecond" name="chasy_no" placeholder="Chassis Number" required>
+                    </div>
+                </div>
+                
+               <div class="form-group">
+                    
+                    <div class="input-group">
+                        <input type="year" class="form-control" id="year" name="year" placeholder="Year of Made:" required>
+                    </div>
+                </div>
+               <div class="form-group">
+                    
+                    <div class="input-group">
+                        <input type="year" class="form-control" id="year" name="brand" placeholder="Vehicle Brand " required>
+                    </div>
+                </div>
+               <div class="form-group">
+                    
+                    <div class="input-group">
+                        <input type="model" class="form-control" id="year" name="model" placeholder="Veicle Model" required>
+                    </div>
+                </div>
+               
+                
+               <div class="form-group">
+        
+        <div class="col-xs-5 selectContainer">
+            <select class="form-control" name="fuel_type">
+                <option value="">Fuel Type</option>
+                <option value="Diesel">Diesel (D)</option>
+                <option value="Petrol">Petrol (P)</option>
+            </select>
+        </div>
+    </div>
+               
+               <div> <input type="submit" name="submit" id="submit" value="Submit" class="btn btn-info bottom-left"></div>
+                
+            </div>
+        </form>
+        <div class="col-lg-5 col-md-push-1">
+            <div class="col-md-12">
+               
+            </div>
+        </div>
+    </div>
+</div>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+    </body>
+
+</html>
